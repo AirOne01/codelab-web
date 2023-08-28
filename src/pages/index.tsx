9124a89d402304e71df7f2a1deacd60fa07e81fe
Erwann LAGOUCHE <popgthyrd@gmail.com>
2023-08-28 17:19:40 +0200

    fix: patch some styles for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <meta name="description" content="Codelab" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Parallax pages={2} className="bg-black text-white font-semibold text-4xl">
+      <Parallax pages={2} className="bg-black text-white font-semibold text-xl md:text-4xl">
         <ParallaxLayer
           speed={.1}
           offset={0}
@@ -35,13 +35,13 @@ export default function Home() {
           </button>
         </ParallaxLayer>
         <ParallaxLayer speed={0.75} offset={0} className="flex flex-col justify-center items-center gap-4">
-          <h1 className="text-9xl font-bold bg-gradient-to-r from-indigo-300 to-purple-400 text-transparent bg-clip-text">Codelab</h1>
+          <h1 className="text-7xl md:text-9xl font-bold bg-gradient-to-r from-indigo-300 to-purple-400 text-transparent bg-clip-text">Codelab</h1>
           {/* aca8fc */}
           <p className="bg-gradient-to-r from-[#ACA8FC] to-[#B991FC] text-transparent bg-clip-text">Coding for fun</p>
         </ParallaxLayer>
-        <ParallaxLayer speed={0.75} offset={1} factor={.2} className="flex flex-col gap-4 justify-center items-center">
+        <ParallaxLayer speed={0.75} offset={1} factor={.2} className="flex flex-col gap-4 justify-center items-center p-1">
           <h2 className="text-5xl">Meet the degenerates</h2>
-          <div className="flex justify-center items-center gap-2">
+          <div className="flex flex-wrap-reverse items-center gap-2">
             {Array.from({ length: 10 }).map((_, i) => (
               <Avatar key={i}>
                 <AvatarImage asChild src="/avatar.jpg">
