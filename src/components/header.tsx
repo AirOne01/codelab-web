c8c88dedc706f5a03ba05940d588e82cf444a7c4
AirOne01 <popgthyrd@gmail.com>
2023-09-23 18:32:33 +0200

    fix(style): change some of the bg

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
-import { UserButton } from "@clerk/nextjs"
-import { Code2Icon } from "lucide-react"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import { Code2Icon, LogInIcon } from "lucide-react"
 import Link from "next/link"
 import * as React from "react"
 
@@ -8,7 +8,7 @@ import { cn } from "~/lib/utils"
 const Header = ({ className, ...props }: { className?: string }) => (
   <div
     className={cn(
-      "flex p-2 w-full shrink-0 overflow-hidden bg-transparent justify-between items-center stroke-white backdrop-blur",
+      "flex p-2 px-3 w-full shrink-0 overflow-hidden bg-transparent justify-between items-center stroke-white backdrop-blur",
       className
     )}
     {...props}
@@ -19,7 +19,17 @@ const Header = ({ className, ...props }: { className?: string }) => (
     </Link>
     {/* RIGHT */}
     <div>
-      <UserButton afterSignOutUrl="/"/>
+      <SignedIn>
+        <UserButton afterSignOutUrl="/"/>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton>
+          <div className="relative group h-8 w-8 cursor-pointer">
+            <LogInIcon className="absolute" />
+            <div className="absolute top-0 left-1/4 h-4 w-4 transition-all duration-150 bg-transparent group-hover:bg-gradient-to-r block blur-md from-purple-400 to-yellow-400" />
+          </div>
+        </SignInButton>
+      </SignedOut>
     </div>
   </div>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import avatar from "../../public/avatar.jpg"
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Header } from "~/components/header";
 import { Blob } from "~/components/three/blob";
+import { Button } from "~/components/ui/button";
 
 export default function Home() {
   return (
@@ -24,13 +25,12 @@ export default function Home() {
           factor={2}
           style={{
             backgroundColor: "rgb(30, 13, 60)",
-            // backgroundColor: "rgb(28, 25, 23)",
-            backgroundImage: "radial-gradient(at 25% 25%, rgb(30, 58, 138) 0, transparent 60%), radial-gradient(at 60% 50%, rgb(112, 26, 117) 0, transparent 50%), radial-gradient(at 90% 100%, rgba(112, 75, 50) 0, transparent 40%), radial-gradient(at 0% 100%, rgb(30, 13, 60) 0, transparent 75%)"
+            backgroundImage: "radial-gradient(at 25% 25%, rgb(15, 29, 69) 0, transparent 60%), radial-gradient(at 60% 50%, rgb(56, 13, 59) 0, transparent 50%), radial-gradient(at 90% 100%, rgba(112, 75, 50) 0, transparent 40%), radial-gradient(at 0% 100%, rgb(30, 13, 60) 0, transparent 75%)"
           }}
         />
         <ParallaxLayer
           speed={1}
-          offset={0.99999}
+          offset={0.999999999}
           factor={1}
           className="bg-gradient-to-b from-transparent to-stone-950"
         />
@@ -47,29 +47,17 @@ export default function Home() {
         </ParallaxLayer>
         <ParallaxLayer speed={1} offset={0} className="flex flex-col justify-center items-center gap-4 dark:text-white text-stone-950">
           <h1 className="text-7xl md:text-9xl font-bold">Codelab</h1>
-          {/* aca8fc */}
           <p>Coding for fun</p>
         </ParallaxLayer>
-        {/* <ParallaxLayer
-          speed={1}
-          offset={0.9999}
-          factor={1.5}
-          style={{
-            backgroundColor: "rgb(30, 13, 60)",
-            // backgroundColor: "rgb(28, 25, 23)",
-            backgroundImage: "radial-gradient(at 25% 25%, rgb(30, 58, 138) 0, transparent 60%), radial-gradient(at 60% 50%, rgb(112, 26, 117) 0, transparent 50%), radial-gradient(at 90% 100%, rgba(112, 75, 50) 0, transparent 40%), radial-gradient(at 0% 100%, rgb(30, 13, 60) 0, transparent 75%)"
-          }}
-        /> */}
-        {/* <ParallaxLayer
-          speed={1}
-          offset={0.9999}
-          // factor={1.5}
-          style={{
-            backgroundColor: "rgb(217, 249, 157)",
-            backgroundImage: "radial-gradient(at 15% 26%, rgb(96, 165, 250) 0, transparent 90%), radial-gradient(at 14% 14%, rgb(134, 239, 172) 0, transparent 56%), radial-gradient(at 68% 6%, rgb(254, 243, 199) 0, transparent 30%), radial-gradient(at 41% 8%, rgb(71, 85, 105) 0, transparent 14%), radial-gradient(at 5% 81%, rgb(254, 226, 226) 0, transparent 77%), radial-gradient(at 45% 17%, rgb(159, 18, 57) 0, transparent 29%)"
-          }}
-        /> */}
-        <ParallaxLayer speed={0.75} offset={1} factor={.2} className="flex flex-col gap-4 justify-center items-center p-1">
+        <ParallaxLayer sticky={{ start: 0 }}>
+          <Header />
+        </ParallaxLayer>
+        <ParallaxLayer
+          speed={0.75}
+          offset={.999999999}
+          factor={1}
+          className="flex flex-col gap-4 justify-center items-center p-1"
+        >
           <h2 className="text-5xl">Meet the degenerates</h2>
           <div className="flex flex-wrap-reverse items-center gap-2">
             {Array.from({ length: 10 }).map((_, i) => (
@@ -81,9 +69,7 @@ export default function Home() {
               </Avatar>
             ))}
           </div>
-        </ParallaxLayer>
-        <ParallaxLayer sticky={{ start: 0 }}>
-          <Header />
+          <Button className="z-50">About</Button>
         </ParallaxLayer>
       </Parallax>
     </>
